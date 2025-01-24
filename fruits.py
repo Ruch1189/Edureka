34fc04314286105c1b5aecf69467d0e39345ccae
Ruch1189 <ruchira.jadhav89@gmail.com>
2025-01-24 09:36:44 -0500

    fruits list

---
+++ fruits.py
@@ -0,0 +1,8 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(len(fruits))
+print(fruits [2:5])
+(fruits.sort(reverse = True))
+print(fruits)
+(fruits.append('grape'))
+(fruits.insert(2, "watermelon"))
+print(fruits)
